665d8cd23538095cb84750f330033aa28fea5b9b
Tsotne Darjania <tsotnedarjania1997@gmail.com>
2023-10-15 12:54:57 +0400

    add header and footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createClient } from "@/prismicio";
+import Link from "next/link";
+import { PrismicNextLink } from "@prismicio/next";
+
+const Footer = async () => {
+  const client = createClient();
+
+  const settings = await client.getSingle("settings");
+
+  return (
+    <footer>
+      <Link href={"/"}>{settings.data.site_title}</Link>
+
+      <p>© {new Date().getFullYear()} </p>
+
+      <ul>
+        {settings.data.navigation.map(({ link, label }) => (
+          <li key={label}>
+            <PrismicNextLink field={link}> {label} </PrismicNextLink>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
